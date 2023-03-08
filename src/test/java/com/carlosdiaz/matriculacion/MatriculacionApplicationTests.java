7a38642d5ff0c0058e221461c021b6dd3f951131
carlosdiaz <cdiapua1107@g.educaand.es>
2023-03-08 08:38:11 +0100

    first commit

---
+++ src/test/java/com/carlosdiaz/matriculacion/MatriculacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosdiaz.matriculacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
